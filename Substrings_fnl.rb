96f9c843b4edd626ef4aa37c0a8bd98b9495936f
Neil Cudden <ncudden@sbcglobal.net>
2017-04-10 13:28:59 -0700

    Substring project added

---
+++ Substrings_fnl.rb
@@ -0,0 +1,24 @@
+dictionary = ["below","down","go","going","horn","how"]
+phrase = "howdy going down"
+ 
+def substrings(ph, di)
+  word_hash = {}
+  words = ph.split(" ")
+  di_length = di.length
+    
+  words.each do |w|
+    count = 0
+    for j in 0..(di_length - 1)
+      search = di[j]
+      if w.include?(search)
+        count += 1
+        word_hash[search] = count
+      else
+        next
+      end
+    end
+  end
+puts "Here\'s the hash:"; word_hash
+end  
+    
+substrings(phrase, dictionary)
